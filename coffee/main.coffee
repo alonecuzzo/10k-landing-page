cb40024cea7739ae06e197370ad27ac4b0033485
Jabari Bell <jabari.bell@pxlflu.net>
2013-02-21 13:42:23 -0500

    added vertical carousel, need to add copy and icons

--- coffee/main.coffee
+++ coffee/main.coffee
@@ -1,4 +1,84 @@
 # main js business
 jQuery ->
-	$('.slides_container').responsiveSlides()
+	currentSlide = 0
+	slideDeltaY = 170
+	slidesYpos0 = -500
+	slidesYpos1 = slidesYpos0 - slideDeltaY
+	slidesYpos2 = slidesYpos1 - slideDeltaY
+	slidesYpos3 = slidesYpos2 - slideDeltaY
+	slideTime = 500
+	fadeOutOpacity = .5
+	$('.slides_container').responsiveSlides(
+		before: ->
+			targetY = 0
+			currentSlide++
+			if currentSlide == 4
+				currentSlide = 0
+			switch currentSlide
+				when 0
+					$('#slides').animate(
+						marginTop: slidesYpos0
+						, slideTime) 
+					$('#callout0').animate(
+						opacity: 1
+						,slideTime)
+					$('#callout1').animate(
+						opacity: fadeOutOpacity
+						,slideTime)
+					$('#callout2').animate(
+						opacity: fadeOutOpacity
+						,slideTime)
+					$('#callout3').animate(
+						opacity: fadeOutOpacity
+						,slideTime)
+				when 1
+					$('#slides').animate(
+						marginTop: slidesYpos1
+						, slideTime)
+					$('#callout0').animate(
+						opacity: fadeOutOpacity
+						,slideTime)
+					$('#callout1').animate(
+						opacity: 1
+						,slideTime)
+					$('#callout2').animate(
+						opacity: fadeOutOpacity
+						,slideTime)
+					$('#callout3').animate(
+						opacity: fadeOutOpacity
+						,slideTime)
+				when 2 
+					$('#slides').animate(
+						marginTop: slidesYpos2
+						, slideTime)
+					$('#callout0').animate(
+						opacity: fadeOutOpacity
+						,slideTime)
+					$('#callout1').animate(
+						opacity: fadeOutOpacity
+						,slideTime)
+					$('#callout2').animate(
+						opacity: 1
+						,slideTime)
+					$('#callout3').animate(
+						opacity: fadeOutOpacity
+						,slideTime)
+				when 3
+					$('#slides').animate(
+						marginTop: slidesYpos3
+						, slideTime)
+					$('#callout0').animate(
+						opacity: fadeOutOpacity
+						,slideTime)
+					$('#callout1').animate(
+						opacity: fadeOutOpacity
+						,slideTime)
+					$('#callout2').animate(
+						opacity: fadeOutOpacity
+						,slideTime)
+					$('#callout3').animate(
+						opacity: 1
+						,slideTime)
+			return
+		)
 	return
--- public/js/main.js
+++ public/js/main.js
@@ -2,7 +2,94 @@
 (function() {
 
   jQuery(function() {
-    $('.slides_container').responsiveSlides();
+    var currentSlide, fadeOutOpacity, slideDeltaY, slideTime, slidesYpos0, slidesYpos1, slidesYpos2, slidesYpos3;
+    currentSlide = 0;
+    slideDeltaY = 170;
+    slidesYpos0 = -500;
+    slidesYpos1 = slidesYpos0 - slideDeltaY;
+    slidesYpos2 = slidesYpos1 - slideDeltaY;
+    slidesYpos3 = slidesYpos2 - slideDeltaY;
+    slideTime = 500;
+    fadeOutOpacity = .5;
+    $('.slides_container').responsiveSlides({
+      before: function() {
+        var targetY;
+        targetY = 0;
+        currentSlide++;
+        if (currentSlide === 4) {
+          currentSlide = 0;
+        }
+        switch (currentSlide) {
+          case 0:
+            $('#slides').animate({
+              marginTop: slidesYpos0
+            }, slideTime);
+            $('#callout0').animate({
+              opacity: 1
+            }, slideTime);
+            $('#callout1').animate({
+              opacity: fadeOutOpacity
+            }, slideTime);
+            $('#callout2').animate({
+              opacity: fadeOutOpacity
+            }, slideTime);
+            $('#callout3').animate({
+              opacity: fadeOutOpacity
+            }, slideTime);
+            break;
+          case 1:
+            $('#slides').animate({
+              marginTop: slidesYpos1
+            }, slideTime);
+            $('#callout0').animate({
+              opacity: fadeOutOpacity
+            }, slideTime);
+            $('#callout1').animate({
+              opacity: 1
+            }, slideTime);
+            $('#callout2').animate({
+              opacity: fadeOutOpacity
+            }, slideTime);
+            $('#callout3').animate({
+              opacity: fadeOutOpacity
+            }, slideTime);
+            break;
+          case 2:
+            $('#slides').animate({
+              marginTop: slidesYpos2
+            }, slideTime);
+            $('#callout0').animate({
+              opacity: fadeOutOpacity
+            }, slideTime);
+            $('#callout1').animate({
+              opacity: fadeOutOpacity
+            }, slideTime);
+            $('#callout2').animate({
+              opacity: 1
+            }, slideTime);
+            $('#callout3').animate({
+              opacity: fadeOutOpacity
+            }, slideTime);
+            break;
+          case 3:
+            $('#slides').animate({
+              marginTop: slidesYpos3
+            }, slideTime);
+            $('#callout0').animate({
+              opacity: fadeOutOpacity
+            }, slideTime);
+            $('#callout1').animate({
+              opacity: fadeOutOpacity
+            }, slideTime);
+            $('#callout2').animate({
+              opacity: fadeOutOpacity
+            }, slideTime);
+            $('#callout3').animate({
+              opacity: 1
+            }, slideTime);
+        }
+      }
+    });
   });
 
 }).call(this);
